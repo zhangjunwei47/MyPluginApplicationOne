5d599fffdc87f7afaab098f7e6bd4bc7bf941ef6
zhangchao <zhangchao1@kaolafm.com>
2018-02-21 06:59:40 +0800

    添加fragment

---
+++ app/src/main/java/com/example/kaola/myrepluginpluginapplication/FirstTestFragment.java
@@ -0,0 +1,33 @@
+package com.example.kaola.myrepluginpluginapplication;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.icu.text.Replaceable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.qihoo360.replugin.RePlugin;
+
+/**
+ * Created by zhaojing on 2018/2/20.
+ */
+
+public class FirstTestFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        /**
+         * 特别注意 需要注意不能使用inflater及container因为他们的Context是宿主的, 但是作为单独的apk. 获取为空.
+         */
+        Context context = RePlugin.getPluginContext();
+        if (context == null) {
+            context = getActivity();
+        }
+        return LayoutInflater.from(context).inflate(R.layout.first_test_fragment, container, false);
+    }
+}
--- app/src/main/java/com/example/kaola/myrepluginpluginapplication/MainActivity.java
+++ app/src/main/java/com/example/kaola/myrepluginpluginapplication/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.kaola.myrepluginpluginapplication;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.support.annotation.Nullable;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +16,29 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.test_view).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                showFragment();
+            }
+        }, 2000);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        return super.onCreateView(name, context, attrs);
+    }
+
+
+    private void showFragment() {
+        FirstTestFragment f1 = new FirstTestFragment();
+        getFragmentManager().beginTransaction().add(R.id.fragment_layout, f1).commit();
     }
 }
